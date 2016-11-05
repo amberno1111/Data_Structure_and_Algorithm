47ef0f9296d0c63ac740eb52b402f6fc3306f6cc
caicai <amberno1111@hotmail.com>
2016-11-05 03:48:20 -0400

    Modify and Add some new

--- LeetCode/Sum_of_Two_Integers.py
+++ LeetCode/Sum_of_Two_Integers.py
@@ -19,15 +19,40 @@ class Solution(object):
         return sum([a, b])
 
 
-class Solution1(object):
+# 这个思路在Python中如果输入是负数的话会遇见问题，在Java中就完全没问题，如果使用Python解决，必须用一个mask来标记才行，参考Solution2
+# class Solution1(object):
+#     def getSum(self, a, b):
+#         """
+#         :type a: int
+#         :type b: int
+#         :rtype: int
+#         """
+#         result, carry = a ^ b, a & b
+#         while carry != 0:
+#             carry <<= 1
+#             result, carry = result ^ carry, result & carry
+#         return result
+
+
+# 思路应该是这样的：
+#   首先把 a 和 b转成补码，然后把两个补码相加，最后转成整数
+#   但是在Python里面，有一个补码标志位，在进行二进制运算的时候可能会丢失这个标志位，比如-1 & 0xffffffff 会变成最大的数4294967295。
+#   转换补码的方式就是，如果有一个数n是负数，则它的补码m = ~(n ^ 0xffffffff),实际上 n = ~(m ^ 0xffffffff)也成立
+class Solution2(object):
     def getSum(self, a, b):
         """
         :type a: int
         :type b: int
         :rtype: int
         """
-        result, carry = a ^ b, a & b
-        while carry != 0:
-            carry <<= 1
-            result, carry = result ^ carry, result & carry
-        return result
+        if a == 0:
+            return b
+        elif b == 0:
+            return a
+        mask = 0xffffffff
+        while b != 0:
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        if (a >> 31) & 1:
+            return ~(a ^ mask)
+        else:
+            return a
